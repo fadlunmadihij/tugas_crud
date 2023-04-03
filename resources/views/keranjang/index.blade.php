bb5a779b54c4df788e1b7c0ecb5e96870d104f4f
fadlunmadihij <fadlunmadihij@gmail.com>
2023-04-03 08:53:38 +0700

    melakukan penjumlahan harga

--- resources/views/keranjang/index.blade.php
+++ resources/views/keranjang/index.blade.php
@@ -4,7 +4,7 @@
           <div class="card">
             <div class="card-header">
                 <h1 class="" style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif">Keranjang</h1>
-              <div class="" style="text-align: center"><input type="text"></div>
+              <div class="" style="text-align: center"><input type="number" id="total" name="totalsemua" onkeyup="InputSub()" readonly></div>
             </div>
             <div class="card-body">
               <table class="table">
@@ -24,11 +24,11 @@
                   <tr>
                     <th scope="row">{{$p++}}</th>
                     <td>{{$item->nama_produk}}</td>
-                    <td>{{$item->harga_produk}}</td>
+                    <td><input type="text" id="harga{{$p}}" value="{{$item->harga_produk}}" name="harga" readonly></td>
                     <td>
-                            <input type="number" name="jumlahbarang">
+                            <input type="text" name="jumlahbarang" id="jumlahbarang{{$p}}" onkeyup="InputSub({{$p}});">
                     </td>
-                    <td><input type="text"></td>
+                    <td><input type="text" id="subtutal{{$p}}" name="subtutal" onkeyup="InputSub();" readonly></td>
                     <td>{{$item->deskripsi}}</td>
                   </tr>
                 </tbody>
@@ -37,4 +37,21 @@
             </div>
           </div>
 </div>
+<script>
+  function InputSub(number_id) {
+        var harga =  parseInt(document.getElementById('harga'+number_id).value);
+        var jumlahbarang =  parseInt(document.getElementById('jumlahbarang'+number_id).value);
+        var hasil = parseInt(harga) * parseInt(jumlahbarang);
+        if (!isNaN(hasil)) {
+          document.getElementById('subtutal'+number_id).value = hasil;
+          console.log(hasil)
+        };
+        var arr = document.getElementsByName('totalsemua');
+        for(var i=0;i<arr.length;i++){
+            if(parseInt(arr[i].value))
+                hasil += parseInt(arr[i].value); 
+            }
+        document.getElementById('total').value = hasil;
+      }
+</script>
 @endsection
